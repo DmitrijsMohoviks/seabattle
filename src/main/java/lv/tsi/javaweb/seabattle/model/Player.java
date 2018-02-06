8362307d822e4d76ea86e14d5168a662af3fb489
Dmitrijs Mohoviks <dmitrijs.mohoviks@gmail.com>
2018-02-07 00:19:08 +0200

    Merge remote-tracking branch 'remotes/upstream/master'
    # Conflicts:
    #	src/main/java/lv/tsi/javaweb/seabattle/controller/RegisterServlet.java
    #	src/main/java/lv/tsi/javaweb/seabattle/controller/ShipPlacementServlet.java
    #	src/main/java/lv/tsi/javaweb/seabattle/controller/WaitEnemyRegisterServlet.java
    #	src/main/java/lv/tsi/javaweb/seabattle/model/Game.java
    #	src/main/java/lv/tsi/javaweb/seabattle/model/GameManager.java
    #	src/main/java/lv/tsi/javaweb/seabattle/model/Player.java
    #	src/main/java/lv/tsi/javaweb/seabattle/model/PlayerGameContext.java
    #	src/main/webapp/WEB-INF/register.jsp
    #	src/main/webapp/WEB-INF/shipPlacement.jsp
    #	src/main/webapp/WEB-INF/waitEnemyRegister.jsp
    #	src/main/webapp/index.html
    #	src/main/webapp/index.jsp
    #	src/main/webapp/main.css

--- src/main/java/lv/tsi/javaweb/seabattle/model/Player.java
+++ src/main/java/lv/tsi/javaweb/seabattle/model/Player.java
@@ -1,14 +1,5 @@
 package lv.tsi.javaweb.seabattle.model;
 
-<<<<<<< HEAD
-public class Player {
-    private String name;
-=======
-/**
- * @author Dimitrijs Fedotovs <a href="http://www.bug.guru">www.bug.guru</a>
- * @version 1.0
- * @since 1.0
- */
 public class Player {
     private String name;
     private Field myField = new Field();
